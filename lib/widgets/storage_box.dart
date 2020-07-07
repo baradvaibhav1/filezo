2fc0a4489503770cf0562cd8cf292627472c080e
AnkitChowdhury <android.catalyst06@gmail.com>
2020-07-08 00:32:33 +0530

    Half way through Storage box

--- lib/widgets/storage_box.dart
+++ lib/widgets/storage_box.dart
@@ -4,11 +4,14 @@ class StorageBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 184,
-      height: 184,
+      width: 192,
+      height: 192,
       child: Material(
         color: const Color(0xff4591F2),
-
+        borderRadius: BorderRadius.circular(12),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+        ),
       ),
     );
   }
