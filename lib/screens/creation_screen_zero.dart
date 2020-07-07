49997e7f18513d901591a296fbcfbbc08469375a
AgntZERO <thundervolt30@gmail.com>
2020-07-07 20:21:16 +0530

    kek

---
+++ lib/screens/creation_screen_zero.dart
@@ -0,0 +1,19 @@
+import 'package:fileexplorer/models/category_item_data.dart';
+import 'package:fileexplorer/widgets/category_button.dart';
+import 'package:fileexplorer/widgets/storage_box.dart';
+import 'package:flutter/material.dart';
+
+class CreationScreenZero extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: StorageBox(),
+        ),
+      ),
+    );
+  }
+}
