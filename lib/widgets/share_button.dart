12a09c3c778bbf3c2aca46b0982653bd15ff0f3f
AnkitChowdhury <ankitdaruler@gmail.com>
2020-07-08 11:46:16 +0530

    Updated Share Button

--- lib/widgets/share_button.dart
+++ lib/widgets/share_button.dart
@@ -3,41 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class ShareButton extends StatelessWidget {
-  const ShareButton({
-    Key key,
-  }) : super(key: key);
+  final double buttonSize;
+  final onTap;
+
+  ShareButton({this.buttonSize = 72, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      width: 72,
-      height: 72,
+    return Container(
       decoration: BoxDecoration(
+        color: Colors.transparent,
         boxShadow: [
           BoxShadow(
-            color: AppColors.shareBtnColorEnd.withOpacity(.7),
-            blurRadius: 5,
-            spreadRadius: 2,
-            offset: Offset(0.0, 2),
-          ),
+            color: AppColors.shareBtnColorEnd,
+            blurRadius: 16.0, // has the effect of softening the shadow
+            spreadRadius: 0.0, // has the effect of extending the shadow
+            offset: Offset(
+              0.0, // horizontal, move right 10
+              16.0, // vertical, move down 10
+            ),
+          )
         ],
-        borderRadius: BorderRadius.circular(18),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            AppColors.shareBtnColorStart,
-            AppColors.shareBtnColorEnd
-          ], // whitish to gray
-          tileMode: TileMode.clamp,
-        ),
       ),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(18),
-        onTap: () {},
-        child: Icon(
-          FlutterIcons.paper_plane_faw5s,
-          color: Colors.white,
+      child: Material(
+        color: Colors.transparent,
+        clipBehavior: Clip.hardEdge,
+        borderRadius: BorderRadius.circular(12),
+        child: Ink(
+          width: buttonSize,
+          height: buttonSize,
+          decoration: BoxDecoration(gradient: new LinearGradient(
+                colors: [
+                  AppColors.shareBtnColorStart,
+                  AppColors.shareBtnColorEnd,
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(1, 1),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: InkWell(
+            child: Icon(
+              FlutterIcons.paper_plane_faw5s,
+              color: Colors.white,
+              size: buttonSize * .45,
+            ),
+            onTap: (){},
+          ),
         ),
       ),
     );
